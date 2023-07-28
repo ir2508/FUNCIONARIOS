49229b553777e964620bc2ad43d201bc6bf8b63d
Igor Roberto <roberto.igor.18@gmail.com>
2023-07-28 01:20:14 -0300

    Criação das etapas de atualizar e remover os funcionários

--- Program.cs
+++ Program.cs
@@ -8,56 +8,118 @@ internal class Program
         string opcao;
 
         var db = new AppDbContext();
-
-        Console.WriteLine("===== BEM-VINDO AO SISTEMA DE FUNCIONÁRIOS =====");
-        Console.WriteLine("[1] Ver Funcionários | [2] Cadastrar Funcionário | [3] Editar Funcionário | [4] Excluir Funcionário");
-        opcao = Console.ReadLine();
-
-        switch (opcao)
+        do
         {
-            case "1":
-                Console.WriteLine("===== FUNCIONÁRIOS CADASTRADOS =====");
-                
-                foreach (var funcionario in db.Funcionario.ToList<Funcionario>())
-                {
-                    Console.WriteLine($"Id: { funcionario.Id } | Nome: { funcionario.Nome } | CPF: { funcionario.Cpf } | Cargo: { funcionario.Cargo } | Nível: { funcionario.NivelExperiencia } | Departamento: { funcionario.Departamento } | Salário: R$ { funcionario.Salario}");
-                }
+
+            Console.WriteLine("\n===== BEM-VINDO AO SISTEMA DE FUNCIONÁRIOS =====");
+            Console.WriteLine("[1] Ver Funcionários | [2] Cadastrar Funcionário | [3] Editar Funcionário | [4] Excluir Funcionário | [-1] Sair");
+            Console.Write("Informe a opção desejada: ");
+            opcao = Console.ReadLine();
+
+            if (opcao == "-1")
+            {
+                Console.WriteLine("==============================================");
+                Console.WriteLine("Obrigado por utilizar nosso sistema. Até mais!");
+                Console.WriteLine("==============================================");
                 break;
+            }
 
-            case "2":
-                // bool novoCadastro = false;
-                // do 
-                // {
-                string nome, cpf, cargo, nivelExperiencia, departamento;
-                decimal salario;
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("\n===== FUNCIONÁRIOS CADASTRADOS =====");
 
-                Console.WriteLine("===== NOVO CADASTRO =====");
-                Console.WriteLine("Informe o nome:");
-                nome = Console.ReadLine();
+                    foreach (var funcionario in db.Funcionario.ToList<Funcionario>())
+                    {
+                        Console.WriteLine($"Id: {funcionario.Id} | Nome: {funcionario.Nome} | CPF: {funcionario.Cpf} | Cargo: {funcionario.Cargo} | Nível: {funcionario.NivelExperiencia} | Departamento: {funcionario.Departamento} | Salário: R$ {funcionario.Salario}");
+                    }
+                    break;
 
-                Console.WriteLine("Informe o cpf:");
-                cpf = Console.ReadLine();
+                case "2":
+                    // bool novoCadastro = false;
+                    // do 
+                    // {
+                    string nome, cpf, cargo, nivelExperiencia, departamento;
+                    decimal salario;
 
-                Console.WriteLine("Informe o salário:");
-                salario = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine("\n===== NOVO CADASTRO =====");
+                    Console.Write("Informe o nome: ");
+                    nome = Console.ReadLine();
 
-                Console.WriteLine("Informe o cargo:");
-                cargo = Console.ReadLine();
+                    Console.Write("Informe o cpf: ");
+                    cpf = Console.ReadLine();
 
-                Console.WriteLine("Informe o nível:");
-                nivelExperiencia = Console.ReadLine();
+                    Console.Write("Informe o salário: ");
+                    salario = Convert.ToDecimal(Console.ReadLine());
 
-                Console.WriteLine("Informe o departamento:");
-                departamento = Console.ReadLine();
+                    Console.Write("Informe o cargo: ");
+                    cargo = Console.ReadLine();
 
-                Funcionario func = new Funcionario(nome, cpf, salario, cargo, nivelExperiencia, departamento);
-                db.Add<Funcionario>(func);
-                // Console.WriteLine("Deseja realizar novo cadastro?");
-                // novoCadastro = Convert.ToBoolean(Console.ReadLine());
+                    Console.Write("Informe o nível: ");
+                    nivelExperiencia = Console.ReadLine();
 
-                // } while(novoCadastro);
-                db.SaveChanges();
-                break;
-        }
+                    Console.Write("Informe o departamento: ");
+                    departamento = Console.ReadLine();
+
+                    Funcionario func = new Funcionario(nome, cpf, salario, cargo, nivelExperiencia, departamento);
+                    db.Add<Funcionario>(func);
+                    // Console.WriteLine("Deseja realizar novo cadastro?");
+                    // novoCadastro = Convert.ToBoolean(Console.ReadLine());
+
+                    // } while(novoCadastro);
+                    break;
+
+                case "3":
+                    int idAtualizar;
+                    Console.Write("\nInforme o Id do funcionário que deseja alterar: ");
+                    idAtualizar = Convert.ToInt16(Console.ReadLine());
+                    Funcionario funcAtualizar = db.Find<Funcionario>(idAtualizar);
+                    while (funcAtualizar == null)
+                    {
+                        Console.Write($"O funcionário com o Id [{idAtualizar}] não foi encontrado... Insira o Id para atualizar: ");
+                        idAtualizar = Convert.ToInt16(Console.ReadLine());
+                        funcAtualizar = db.Find<Funcionario>(idAtualizar);
+                    }
+
+                    Console.Write("Informe o nome: ");
+                    funcAtualizar.Nome = Console.ReadLine();
+
+                    Console.Write("Informe o cpf: ");
+                    funcAtualizar.Cpf = Console.ReadLine();
+
+                    Console.Write("Informe o salário: ");
+                    funcAtualizar.Salario = Convert.ToDecimal(Console.ReadLine());
+
+                    Console.Write("Informe o cargo: ");
+                    funcAtualizar.Cargo = Console.ReadLine();
+
+                    Console.Write("Informe o nível: ");
+                    funcAtualizar.NivelExperiencia = Console.ReadLine();
+
+                    Console.Write("Informe o departamento: ");
+                    funcAtualizar.Departamento = Console.ReadLine();
+
+                    break;
+
+                case "4":
+                    int idRemover;
+                    Console.Write("\nInforme o Id do funcionário que deseja remover: ");
+                    idRemover = Convert.ToInt16(Console.ReadLine());
+                    Funcionario funcRemover = db.Find<Funcionario>(idRemover);
+
+                    while (funcRemover == null)
+                    {
+                        Console.Write($"O funcionário com o Id [{idRemover}] não foi encontrado... Insira o Id para remoção: ");
+                        idRemover = Convert.ToInt16(Console.ReadLine());
+                        funcRemover = db.Find<Funcionario>(idRemover);
+                    }
+
+                    Console.WriteLine($"Removendo o funcionário {funcRemover.Nome}...");
+                    db.Remove(funcRemover);
+                    break;
+            }
+            
+            db.SaveChanges();
+        } while (opcao != "-1");
     }
 }
